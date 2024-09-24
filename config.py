faa5669f057d98861d565df97a52e39b3cb1e6b2
VaABB <36303293+Vahe327@users.noreply.github.com>
2024-09-24 10:51:30 +0400

    Update config.py

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'your_secret_key')
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///scrats_game.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    API_TOKEN = os.getenv('API_TOKEN', '7339241159:AAHGwH1NA15t49-YQxwkD3hOdxWxqKF6bsc')
+    API_TOKEN = os.getenv('API_TOKEN', 'Token')
